2b9f6de0d9984007d91503fe0e584f4141334b9b
Derek Foster <derekfoster94@gmail.com>
2021-02-08 15:19:20 -0800

    initial pagination + layout styling

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,32 @@
 import Head from "next/head";
 import { Octokit } from "@octokit/core";
 import { useState, useEffect } from "react";
+import { Pagination } from "react-bootstrap";
 
 export default function Home() {
   const [emojis, setEmojis] = useState([]);
+  const [page, setPage] = useState({ size: 10, number: 0 });
+  const [displayedEmojis, setDisplayedEmojis] = useState([]);
 
   useEffect(async () => {
     const octokit = new Octokit();
     const response = await octokit.request("GET /emojis");
 
-    setEmojis(Object.entries(response.data));
+    const returnedEmojis = Object.entries(response.data);
+
+    setEmojis(returnedEmojis);
+    setDisplayedEmojis(returnedEmojis.slice(0, page.size));
+    setPage({
+      size: page.size,
+      number: Math.ceil(returnedEmojis.length / page.size)
+    });
   }, []);
+
+  const tableStyle = {
+    display: "flex",
+    justifyContent: "space-between",
+    gap: "80px"
+  };
 
   return (
     <>
@@ -21,10 +37,23 @@ export default function Home() {
 
       <main>
         <h1> Flow Labs frontend interview</h1>
-        <div>
-          {emojis.map(emoji => (
-            <div>
-              <h3>{emoji[0]}</h3>
+
+        <Pagination>
+          <Pagination.First />
+          <Pagination.Prev />
+
+          <Pagination.Next />
+          <Pagination.Last />
+        </Pagination>
+        <div style={{ width: "400px", margin: "0 auto" }}>
+          <div style={tableStyle}>
+            <h3>Name</h3>
+            <h3>Emoji</h3>
+          </div>
+
+          {displayedEmojis.map(emoji => (
+            <div style={tableStyle}>
+              <h4>{emoji[0]}</h4>
               <img src={emoji[1]} alt="" />
             </div>
           ))}
@@ -33,3 +62,17 @@ export default function Home() {
     </>
   );
 }
+
+/*
+<Pagination.Item>{1}</Pagination.Item>
+<Pagination.Ellipsis />
+
+<Pagination.Item>{10}</Pagination.Item>
+<Pagination.Item>{11}</Pagination.Item>
+<Pagination.Item active>{12}</Pagination.Item>
+<Pagination.Item>{13}</Pagination.Item>
+<Pagination.Item disabled>{14}</Pagination.Item>
+
+<Pagination.Ellipsis />
+<Pagination.Item>{20}</Pagination.Item>
+*/
